1a95d6105a292034eca21084366af1eebb4b78fb
Antonio Ruggiero <antonio.ruggiero@uqido.com>
2021-01-31 15:40:55 +0100

    Screenshots!!!!!!

--- Assets/Scripts/EndingManager.cs
+++ Assets/Scripts/EndingManager.cs
@@ -1,16 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndingManager : MonoBehaviour
 {
+    [SerializeField] private CanvasGroup screenshotButtonPanel;
+    [SerializeField] private Button screenshotButton;
+    [SerializeField] private Button backToMenuButton;
 
-
-    public void ShowResult()
+    public void ShowScreenShotButton()
     {
-        
+        screenshotButtonPanel.interactable = true;
+        screenshotButtonPanel.blocksRaycasts = true;
+        screenshotButtonPanel.DOFade(1, 0.25f);
+        screenshotButton.onClick.AddListener(Screenshot);
     }
 
+
+    private void Screenshot()
+    {
+        screenshotButtonPanel.alpha = 0;
+        backToMenuButton.enabled = false;
+        screenshotButton.interactable = false;
+        var folderPath = Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop);
+        var completePath = Path.Combine(folderPath, $"Fast_n_Funeral_{DateTime.Now.ToLongTimeString().Replace(":","_")}.png");
+        Debug.Log(completePath);
+        ScreenCapture.CaptureScreenshot(completePath);
+        screenshotButtonPanel.alpha = 1;
+        backToMenuButton.enabled = true;
+    }
 
     public void GoToMenu()
     {
