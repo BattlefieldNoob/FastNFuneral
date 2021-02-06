0ee6bcef5b1c21f7adff31ff25f6862bd16c91e9
Antonio Ruggiero <ant.ruggiero16@gmail.com>
2021-02-06 22:52:40 +0100

    fix on change scene fade and ending timeline

--- Assets/Scripts/Managers/GameCanvasManager.cs
+++ Assets/Scripts/Managers/GameCanvasManager.cs
@@ -8,13 +8,7 @@ using UnityEngine.SceneManagement;
 public class GameCanvasManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text timerText;
-    [SerializeField] private string endingScene;
     
-    private void Start()
-    {
-        EventManager.Instance.OnCountdownEnd.AddListener((x)=>SceneManager.LoadScene(endingScene));
-    }
-
     private void Update()
     {
         timerText.text = GetTimerString(CountdownManager.Instance.GetRemainingSeconds);
